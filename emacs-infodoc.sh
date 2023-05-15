885cc7980c35e0415b3c7edc240c85c9051237cd
gustavopuche <gustavo.puche@gmail.com>
2023-05-15 16:01:20 +0200

    New

--- emacs-infodoc.sh
+++ emacs-infodoc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i "s/(load-theme.*/(load-theme 'doom-one-light t)/1" ~/dev/emacs-extensions/emacs-themes/themes-setup.el
-sed -i "32 s/(load-theme.*/(load-theme 'infodoc t)/g" ~/dev/emacs-extensions/emacs-themes/themes-setup.el
+sed -i "28 s/(load-theme.*/(load-theme 'infodoc t)/g" ~/dev/emacs-extensions/emacs-themes/themes-setup.el
 /usr/bin/emacs
