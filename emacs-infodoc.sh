0fec04da5e0a1cc2bf606287eaf4e446b9ee3700
Gustavo Puche <gustavo.puche.ext@siemensgamesa.com>
2023-05-30 12:07:00 +0200

    Change infodoc to gus-infodoc

--- emacs-infodoc.sh
+++ emacs-infodoc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i "s/(load-theme.*/(load-theme 'doom-one-light t)/1" ~/dev/emacs-extensions/emacs-themes/themes-setup.el
-sed -i "27 s/(load-theme.*/(load-theme 'infodoc t)/g" ~/dev/emacs-extensions/emacs-themes/themes-setup.el
+sed -i "27 s/(load-theme.*/(load-theme 'gus-infodoc t)/g" ~/dev/emacs-extensions/emacs-themes/themes-setup.el
 /usr/bin/emacs
