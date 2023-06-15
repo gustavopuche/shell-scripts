57e4b41cb5b783a7aa41f0b4b914120a62ab3091
Gustavo Puche <gustavo.puche.ext@siemensgamesa.com>
2023-06-15 13:34:31 +0200

    new dtterm yellow

---
+++ dtterm-yellow-top.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/dt/bin/dtterm -fg "#919191" -bg "#eded64" -fn "-*-pt mono-*-*-*-*-*-*-*-*-*-*-*-*" -e $1
--- emacs-gus-blue-yellow.sh
+++ emacs-gus-blue-yellow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i "s/(load-theme.*/(load-theme 'doom-monokai-spectrum t)/1" ~/dev/emacs-extensions/emacs-themes/themes-setup.el
 sed -i "27 s/(load-theme.*/(load-theme 'gus-blue-yellow t)/g" ~/dev/emacs-extensions/emacs-themes/themes-setup.el
-/usr/bin/emacs
+emacs-30.0.50
